bd8e9e206350fdbfdbbf60bc7b6712ac11c01233
komcal <kom.sujautra@gmail.com>
2016-12-21 20:13:08 +0700

    gold hit

--- models.py
+++ models.py
@@ -1,9 +1,13 @@
+from random import randint
+
 class Model:
     def __init__(self, world, x, y, angle):
         self.world = world
         self.x = x
         self.y = y
         self.angle = 0
+    def hit(self, other, hit_size):
+        return (abs(self.x - other.x) <= hit_size) and (abs(self.y - other.y) <= hit_size)
         
 class Ship(Model):
     def __init__(self, world, x, y):
@@ -41,6 +45,9 @@ class World:
     def animate(self, delta):
         self.ship.animate(delta)
         
+        if self.ship.hit(self.gold, 15):
+            self.gold.random_location()
+        
     def on_key_press(self, key, key_modifiers):
         if key == arcade.key.SPACE:
             self.ship.switch_direction()
@@ -51,3 +58,7 @@ class Gold(Model):
         super().__init__(world, x, y, 0)
         self.x = x
         self.y = y
+        
+    def random_location(self):
+        self.x = randint(0, self.world.width - 1)
+        self.y = randint(0, self.world.height - 1)
