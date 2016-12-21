0dbc7dd57426c7efd7aab7e330f7bca233f0932a
komcal <kom.sujautra@gmail.com>
2016-12-21 21:04:27 +0700

    ship move

--- models.py
+++ models.py
@@ -14,33 +14,37 @@ class Model:
         return (abs(self.x - other.x) <= hit_size) and (abs(self.y - other.y) <= hit_size)
         
 class Ship(Model):
-    DIR_HORIZONTAL = 0
-    DIR_VERTICAL = 1
+    DIR_LEFT = 0
+    DIR_UP = 1
+    DIR_RIGHT = 2
+    DIR_DOWN = 3
+    ANGLE = [90, 0, -90, 180]
     
     def __init__(self, world, x, y):
         self.world = world
         super().__init__(world, x, y, 0)
         self.x = x
         self.y = y
-        self.direction = Ship.DIR_VERTICAL
+        self.direction = Ship.DIR_DOWN
+        self.angle = Ship.ANGLE[3]
  
-    def switch_direction(self):
-        if self.direction == Ship.DIR_HORIZONTAL:
-            self.direction = Ship.DIR_VERTICAL
-            self.angle = 0
-        else:
-            self.direction = Ship.DIR_HORIZONTAL
-            self.angle = -90 
- 
+    def switch_direction(self, direction):
+        self.direction = direction
+        self.angle = Ship.ANGLE[direction]
+        
     def animate(self, delta):
-        if self.direction == Ship.DIR_VERTICAL:
-            if self.y > self.world.height:
-                self.y = 0
-            self.y += 5
-        else:
-            if self.x > self.world.width:
-                self.x = 0
-            self.x += 5
+        if self.direction == Ship.DIR_LEFT:
+            if self.x > 5:
+                self.x -= 5
+        if self.direction == Ship.DIR_RIGHT:
+            if self.x < self.world.width:
+                self.x += 5
+        if self.direction == Ship.DIR_UP:
+            if self.y < self.world.height:
+                self.y += 5
+        if self.direction == Ship.DIR_DOWN:
+            if self.y > 5:
+                self.y -= 5
             
 class World:
     def __init__(self, width, height):
@@ -58,8 +62,14 @@ class World:
             self.score += 1
         
     def on_key_press(self, key, key_modifiers):
-        if key == arcade.key.SPACE:
-            self.ship.switch_direction()
+        if key == arcade.key.LEFT:
+            self.ship.switch_direction(0)
+        if key == arcade.key.UP:
+            self.ship.switch_direction(1)
+        if key == arcade.key.RIGHT:
+            self.ship.switch_direction(2)
+        if key == arcade.key.DOWN:
+            self.ship.switch_direction(3)
             
 class Gold(Model):
     def __init__(self, world, x, y):
